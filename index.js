f04ef694fedfcb80d0275f5b0c669cc52d3f63f2
ywhp <100523022+ywhp@users.noreply.github.com>
2025-08-03 23:35:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client , GatewayIntentBits , AttachmentBuilder, Collection} = require('discord.js');
+const { Client , GatewayIntentBits } = require('discord.js');
 
 const fs = require('fs');
 
